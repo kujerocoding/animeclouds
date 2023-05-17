bd067777679b867fe93409568107fa451a029023
herom <jerome.manset22@gmail.com>
2023-05-17 23:24:42 +0800

    Rendered elements to the Airing and Upcoming components

--- src/components/Airing.jsx
+++ src/components/Airing.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import { useGlobalContext } from '../context/global'
 
 const Airing = () => {
+
+  const {airingAnime, isSearch} = useGlobalContext()
+  
+ 
+  const conditionalRender = () => {
+    if(!isSearch){
+      return airingAnime.map(anime => {
+        //console.log(anime)
+        return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+        <img src={anime.images.jpg.large_image_url} alt="" className='w-full h-full object-cover'/>
+        </Link>
+      })
+    }else{
+      alert('No anime found')
+    }
+  }
+
+
   return (
-    <div>
-      airing
+    <div className='border-2 border-red-500'>
+      <div className='grid grid-cols-auto gap-4 border-2 border-blue-500'>
+      {conditionalRender()}
+      </div>
     </div>
   )
 }
 
 export default Airing
+
