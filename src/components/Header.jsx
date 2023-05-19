b50ce8b207d326eff8b52c8a5d7c449e43984475
herom <jerome.manset22@gmail.com>
2023-05-20 02:51:27 +0800

    Import logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useGlobalContext } from '../context/global'
 import {BsSearch} from 'react-icons/bs'
 import { Link } from 'react-router-dom'
+import Logo from '../assets/images/logo.png'
 
 const Header = ({setRendered}) => {
 
@@ -11,7 +12,7 @@ const Header = ({setRendered}) => {
     <header className='text-center'>
         <nav className='w-11/12 mx-auto py-4 flex items-center justify-between'>
             <Link onClick={() => window.location.reload(true)} className='w-40'>
-                <img src="./src/assets/images/logo.png" alt="logo" />
+                <img src={Logo} alt="logo" />
             </Link>
             <form action="" className="flex bg-primary-btn pr-4 shadow ">
                     <input className="appearance-none border w-full py-1 px-2 text-secondary-300 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder='Search Anime' onChange={handleChange} value={search}/>
